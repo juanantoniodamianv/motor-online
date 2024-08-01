ab05ad6780293c8473260eacb9a22efd2f9816fb
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2024-08-01 18:09:40 +1000

    Update migration for triggers

---
+++ supabase/migrations/20240801080810_remote_schema.sql
@@ -0,0 +1,84 @@
+create table "public"."users" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "name" text not null,
+    "username" text,
+    "avatar_url" text,
+    "email" text not null
+);
+
+
+alter table "public"."users" enable row level security;
+
+alter table "public"."provinces" enable row level security;
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."users" add constraint "users_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."users" validate constraint "users_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.insert_user_in_public_table_for_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$BEGIN
+  INSERT INTO public.users (id, name, username, avatar_url, email)
+  VALUES (
+    new.id,
+    new.raw_user_meta_data ->> 'full_name',
+    new.raw_user_meta_data ->> 'name',
+    new.raw_user_meta_data ->> 'avatar_url',
+    new.raw_user_meta_data ->> 'email'
+  );
+  RETURN NEW;
+END;$function$
+;
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant references on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant trigger on table "public"."users" to "anon";
+
+grant truncate on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant references on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant trigger on table "public"."users" to "authenticated";
+
+grant truncate on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant references on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant trigger on table "public"."users" to "service_role";
+
+grant truncate on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+
