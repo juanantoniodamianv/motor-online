f8c229d973fddb9040e96ee386190e7c824ce5d7
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2024-08-28 10:47:59 +1000

    Update login page

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -8,6 +8,40 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen min-h-screen flex items-center justify-center">
       <div className="w-full max-w-sm items-center bg-white p-8 rounded-lg shadow-md space-y-4">
+        <form className="mt-4">
+          <div className="flex justify-center mt-4">
+            <button
+              formAction={signInWithGoogle}
+              className="bg-red-500 text-white w-full py-2 rounded hover:bg-red-600 flex items-center justify-center space-x-4"
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="google"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                ></path>
+              </svg>
+              <span>Continuar con Google</span>
+            </button>
+          </div>
+        </form>
+
+        <div className="flex items-center justify-center">
+          <div className="flex items-center w-full">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="px-4 text-gray-500">también puedes</span>
+            <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+        </div>
+
         {newUser ? (
           <form className="">
             <div className="md:flex md:items-center mb-4">
@@ -164,40 +198,6 @@ export default function LoginPage() {
             </div>
           </form>
         )}
-
-        <div className="flex items-center justify-center">
-          <div className="flex items-center w-full">
-            <div className="flex-grow border-t border-gray-300"></div>
-            <span className="px-4 text-gray-500">también puedes</span>
-            <div className="flex-grow border-t border-gray-300"></div>
-          </div>
-        </div>
-
-        <form className="mt-4">
-          <div className="flex justify-center mt-4">
-            <button
-              formAction={signInWithGoogle}
-              className="bg-red-500 text-white w-full py-2 rounded hover:bg-red-600 flex items-center justify-center space-x-4"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="google"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 488 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                ></path>
-              </svg>
-              <span>Continuar con Google</span>
-            </button>
-          </div>
-        </form>
       </div>
     </div>
   );
