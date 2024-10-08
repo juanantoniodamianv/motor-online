75948b73e43a6a3c15314bba830374fe9229658e
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2024-10-08 10:41:52 +1000

    temporary fix

--- src/app/components/sign-out-button.tsx
+++ src/app/components/sign-out-button.tsx
@@ -2,17 +2,29 @@
 
 import { useAuthContext } from "@/src/context/auth-context";
 import { Sidebar } from "flowbite-react";
+import { useRouter } from "next/navigation";
 import { HiLogout } from "react-icons/hi";
 
 export default function SignOutButton() {
   const { handleSignOut } = useAuthContext();
+  const router = useRouter();
+
+  // TODO: need to fix this, after signing out refreshes the site properly
+  const onSignOut = async () => {
+    try {
+      await handleSignOut();
+      router.refresh();
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
 
   return (
     <Sidebar.Item
       href="#"
       icon={HiLogout}
       className="flex justify-start"
-      onClick={handleSignOut}
+      onClick={onSignOut}
     >
       Cerrar Sesión
     </Sidebar.Item>
